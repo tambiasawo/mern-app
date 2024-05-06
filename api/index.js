69a967972866e66cbe478287f38cce0b3fbec04c
Tambi <tambi.asawo@verticalmotion.ca>
2024-05-06 13:10:48 -0700

    feat: install and setup mongodb

--- api/index.js
+++ api/index.js
@@ -1,6 +1,18 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+//X9dr2OJcaMnjyFnr
+dotenv.config();
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(function () {
+    console.log("mongodb connected");
+  })
+  .catch((e) => {
+    console.log("an error occured", e.message);
+  });
 const app = express();
 
-app.listen(3000, ()=>{
-    console.log('listening on port 3000!')
-})
+app.listen(3000, () => {
+  console.log("listening on port 3000!");
+});
