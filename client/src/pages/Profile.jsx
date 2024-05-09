d07060bca6f3dd5c7f43b505cb8d690d3015dd16
Tambi <tambi.asawo@verticalmotion.ca>
2024-05-09 13:42:18 -0700

    feat: add upload image functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,19 +1,29 @@
 import React from "react";
-import OAuthBtn from "../components/OAuthBtn";
 import { Link, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
+import { app } from "../../firebase";
 import {
   signInStart,
   signInFailure,
   signInSuccess,
 } from "../redux/user/userSlice";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
 
 function Profile() {
   const [formData, setFormData] = React.useState({});
-
+  const fileRef = React.useRef(null);
+  const [image, setImage] = React.useState(null);
+  const [imageUploadProgress, setImageUploadProgress] = React.useState(0);
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const { currentUser } = useSelector((state) => state.user);
+  const [imageError, setImageError] = React.useState(false);
+  //const [formData, setFormData] = React.useState({});
 
   const handleChange = (e) => {
     setFormData((prev) => {
@@ -44,19 +54,68 @@ function Profile() {
       dispatch(signInFailure(error));
     }
   };
+
+  const handleImageChange = (e) => {
+    const image = e.target.files[0];
+    setImage(image);
+  };
+
+  React.useEffect(() => {
+    handleImageUpload();
+  }, [image]);
+
+  const handleImageUpload = async () => {
+    const storage = getStorage(app);
+    const fileName = image.name + new Date().getTime();
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, image);
+
+    uploadTask.on("state_changed", (snapshot) => {
+      const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      setImageUploadProgress(Math.round(progress));
+    });
+    (error) => setImageError(true);
+    () => {
+      getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+        setFormData((prev) => ({ ...prev, profileImage: downloadURL }));
+      });
+    };
+  };
+
   return (
     <div className="mx-auto p-3 mt-10">
       <h1 className="text-center font-semibold text-3xl py-3"> Profile</h1>
-
       <form
         className="flex justify-center items-center flex-col gap-4"
         onSubmit={handleSubmit}
       >
+        <input
+          type="file"
+          ref={fileRef}
+          hidden
+          accept="image/*"
+          onChange={handleImageChange}
+        />
+
         <img
-          src={currentUser.profileImage}
+          src={formData.profileImage || currentUser.profileImage}
           alt="profile image"
           className="h-24 w-24 rounded-full self-center cursor-pointer object-cover mb-3"
+          onClick={() => fileRef.current.click()}
         />
+        <p className="text-sm self-center">
+          {imageError ? (
+            <span className="text-red-700">
+              Error uploading image (file size must be less than 2 MB)
+            </span>
+          ) : imageUploadProgress > 0 && imageUploadProgress < 100 ? (
+            <span className="text-slate-700">{`Uploading: ${imageUploadProgress} %`}</span>
+          ) : imageUploadProgress === 100 ? (
+            <span className="text-green-700">Image uploaded successfully</span>
+          ) : (
+            ""
+          )}
+        </p>
         <input
           type="username"
           id="username"
@@ -82,7 +141,7 @@ function Profile() {
           className="bg-slate-700 rounded-lg p-3 w-full sm:w-[75%] lg:w-[35%] disabled:opacity-70 hover:opacity-95 uppercase text-white"
           disabled={currentUser.loading}
         >
-          {currentUser.loading ? "Loading..." : "Update"}
+          {currentUser.loading ? "Updating..." : "Update"}
         </button>
 
         <div className="flex justify-between w-full sm:w-[75%] lg:w-[35%]">
