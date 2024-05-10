6d897b01f0c9a7d663c80b678b2ccba1b4fac625
Tambi <tambi.asawo@verticalmotion.ca>
2024-05-10 13:17:11 -0700

    chore: prepare build for production

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import errorHandler from "./utils/errorHandler.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 dotenv.config();
 mongoose
@@ -16,6 +17,13 @@ mongoose
     console.log("an error occured", e.message);
   });
 const app = express();
+const __dirname = path.resolve();
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use(express.json());
 app.use(cookieParser());
