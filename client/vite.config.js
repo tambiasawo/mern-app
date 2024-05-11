d952812d5dcc90b0ebb690c7e483239a13b9a8dc
Tambi <tambi.asawo@verticalmotion.ca>
2024-05-10 17:15:09 -0700

    fix: 405 error encountered in production

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,9 +4,9 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
+  /*  server: {
     proxy: {
       "/api": { target: "http://localhost:3000", secure: false }, //will use localhost:5173 instead of 3000
     },
-  },
+  }, */
 });
