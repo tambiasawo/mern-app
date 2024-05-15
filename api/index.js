b57a760f1c5f5ab8fbb95260afc86818ad6bba51
Tambi <tambi.asawo@verticalmotion.ca>
2024-05-14 18:06:41 -0700

    fix: build issues

--- api/index.js
+++ api/index.js
@@ -18,8 +18,9 @@ mongoose
   });
 const app = express();
 const __dirname = path.resolve();
-//app.use(express.static(path.join(__dirname, "/client/dist")));
-app.use(express.static("dist"));
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
