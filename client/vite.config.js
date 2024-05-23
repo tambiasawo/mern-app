e1b4a967bd9e6b54abaf96c2b249d134866a390b
Tambi <tambi.asawo@verticalmotion.ca>
2024-05-23 15:21:59 -0700

    chore: config vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,8 +5,8 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
   server: {
-  /*   proxy: {
+    proxy: {
       "/api": { target: "http://localhost:3000", secure: false }, // will use localhost:5173 instead of 3000
-    }, */
+    },
   },
 });
