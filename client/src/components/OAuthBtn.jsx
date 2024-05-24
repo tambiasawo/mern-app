869de896b317ff5b7353d21861acca411d7d78ea
Tambi <tambi.asawo@verticalmotion.ca>
2024-05-24 13:28:58 -0700

    fix: remove base url

--- client/src/components/OAuthBtn.jsx
+++ client/src/components/OAuthBtn.jsx
@@ -13,7 +13,7 @@ function OAuthBtn() {
       const provider = new GoogleAuthProvider();
       const auth = getAuth(app);
       const result = await signInWithPopup(auth, provider);
-      const response = await fetch("http://localhost:3000/api/auth/google/", {
+      const response = await fetch("/api/auth/google/", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
